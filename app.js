439aa483d7afc35a2f1e949ff277874a181a42ea
shopatlemo <shopatlemo@gmail.com>
2023-07-29 13:59:35 -0400

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Imports
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 // Static Files
 app.use(express.static('public'));
